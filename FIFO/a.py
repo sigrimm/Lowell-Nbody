406c3c9b9bc5303312fad5c7850766978fbc080f
sigrimm <simon.l.grimm@gmail.com>
2022-12-20 17:09:28 +0100

    renamed Run directory

--- FIFO/a.py
+++ FIFO/a.py
@@ -12,24 +12,23 @@ try:
 except:
 	pass
 
-data = np.ones(2)
-data[0] = 5.0
-data[1] = 6.0
+N = 1
 
-
-#fifo = open(path, "w")
-
+#################################
+# send N
+#################################
 fd = os.open('myfifo', os.O_WRONLY)
 
-line = data.tobytes()
+line = N.to_bytes(2, 'little')
 
 os.write(fd, line)
 
 os.close(fd)
-
+print('Sent', N)
 
 ##############################
-OK = np.zeros(1)
+# receive N to check
+##############################
 
 fd = open('fifoCheck', 'r')
 
@@ -37,8 +36,35 @@ line2 = fd.read()
 
 line3= str.encode(line2)
 
-d = int.from_bytes(line3, byteorder='little', signed=False)
-print(d)
+N2 = int.from_bytes(line3, byteorder='little', signed=False)
+print(N2)
 
 fd.close()
+
+##############################
+# read initial conditions
+##############################
+
+time1, index, x, y, z, vx, vy, vz = np.loadtxt('initial1.dat', dtype='double', unpack=True)
+
+fd = os.open('myfifo', os.O_WRONLY)
+
+time = 2396770.5
+
+print(time, index, x, y, z, vx, vy, vz)
+
+#line = data.to_bytes(4, 'little')
+line = bytearray(struct.pack("d", time1))
+line += bytearray(struct.pack("i", int(index)))
+line += bytearray(struct.pack("d", x))
+line += bytearray(struct.pack("d", y))
+line += bytearray(struct.pack("d", z))
+line += bytearray(struct.pack("d", vx))
+line += bytearray(struct.pack("d", vy))
+line += bytearray(struct.pack("d", vz))
+
+os.write(fd, line)
+
+os.close(fd)
+
 
